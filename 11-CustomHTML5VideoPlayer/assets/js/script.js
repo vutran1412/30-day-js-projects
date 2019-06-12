7c8668c4097195d2effe7f1bd4ce264062bf3669
vutran <vutran1412@icloud.com>
2019-06-11 18:08:56 -0700

    add fullscreen button to video player

--- 11-CustomHTML5VideoPlayer/assets/js/script.js
+++ 11-CustomHTML5VideoPlayer/assets/js/script.js
@@ -5,6 +5,7 @@ const progressBar = player.querySelector('.progress__filled')
 const toggle = player.querySelector('.toggle')
 const skipButtons = player.querySelectorAll('[data-skip]')
 const ranges = player.querySelectorAll('.player__slider')
+const fullscreen = player.querySelector('.fullscreen')
 
 function togglePlay() {
     const method = video.paused ? 'play' : 'pause'
@@ -36,10 +37,16 @@ function handleProgressUpdate(e) {
     video.currentTime = time
 }
 
+function makeFullScreen() {
+    video.webkitRequestFullScreen()
+}
+
+
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
 video.addEventListener('timeupdate', handleProgress)
+
 
 toggle.addEventListener('click', togglePlay)
 ranges.forEach(slider => slider.addEventListener('change', handleRangeUpdate))
@@ -51,3 +58,5 @@ progress.addEventListener('click', handleProgressUpdate)
 progress.addEventListener('mousemove', (e) => mousedown && handleProgressUpdate(e))
 progress.addEventListener('mousedown', () => mousedown = true)
 progress.addEventListener('mouseup', () => mousedown = false)
+
+fullscreen.addEventListener('click', makeFullScreen)
